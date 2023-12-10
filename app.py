20d539acad3d8a6e2b5d8ca55605ddb2540f2867
GirijaManoj <kgmkr00001@gmail.com>
2023-12-10 10:27:55 -0600

    Removing the debug so that it wont log the landing page

--- app.py
+++ app.py
@@ -589,4 +589,4 @@ def update_product_in_db(product_id, product_data):
     return True
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
