6bd66c017677b552dc5eadb46112952c81029f1c
kukr <kudaykanth17@yahoo.in>
2024-02-02 18:53:08 +0530

    Fixing errors

--- aws_config.py
+++ aws_config.py
@@ -1,11 +1,11 @@
 import boto3
 import json
+import os
 
-# authenticate with AWS from json file
-with open('aws_config.json') as f:
-    data = json.load(f)
-    ACCESS_KEY = data['access_key']
-    SECRET_KEY = data['secret']
+# authenticate with AWS from .env file
+ACCESS_KEY = os.getenv('AWS_ACCESS_KEY')
+SECRET_KEY = os.getenv('AWS_SECRET_KEY')
+
 
 s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,
                     aws_secret_access_key=SECRET_KEY)
